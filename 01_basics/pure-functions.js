3a7076c69f16bf0bc120a28cb70e39f6f7805f72
chaudhryhammad <101020719+chaudhryhammad@users.noreply.github.com>
2024-05-21 17:22:14 +0000

    practice

--- 01_basics/pure-functions.js
+++ 01_basics/pure-functions.js
@@ -1,1 +1,30 @@
-console.log()
+// function myName()
+// {
+//     console.log("H");
+//     console.log("A");
+//     console.log("M");
+//     console.log("M");
+//     console.log("A");
+//     console.log("D");
+// }
+// myName()    
+
+function addNumber(num1 , num2)
+{
+    return num1 + num2
+}
+const result = addNumber(5,7) 
+console.log("Result " , result);
+
+
+
+function loginusername(username){
+if(!username)
+{
+    console.log("please enter a usrname: ")
+    return  
+}
+
+}
+
+console.log(loginusername("Hammad"));
